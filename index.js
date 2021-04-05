f882ace083e45938d07232eebd221d87e0888f00
Krzysztof Kowerczyk <krzysztof.kowerczyk@gmail.com>
2021-04-05 11:56:55 +0200

    footerchanges

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ function setCopyright() {
     let currentDate = new Date();
     let currentYear = currentDate.getFullYear();
     if (currentYear === 2021) {
-        document.getElementById("copyright").innerHTML = "<p><b>copyright &#169 " + currentYear + " zbigniew-kowerczyk.pl</b></p>";
+        document.getElementById("copyright").innerHTML = "<div><b><pre><b>copyright &#169 " + currentYear + "</b></pre><pre><b>\nzbigniew-kowerczyk.pl</b></pre></b></div>";
     } else {
-        document.getElementById("copyright").innerHTML = "<p><b>copyright &#169 2021 - " + currentYear + " zbigniew-kowerczyk.pl</b></p>";
+        document.getElementById("copyright").innerHTML = "<div><b><pre><b>copyright &#169 2021 - " + currentYear + "</b></pre><pre><b>\nzbigniew-kowerczyk.pl</b></pre></b></div>";
     }
 }
 setCopyright();
