6e858a1192059bcded85c53555de5dca0cfc05ea
Krzysztof Kowerczyk <krzysztof.kowerczyk@gmail.com>
2021-05-13 18:48:00 +0200

    okladka

--- index.js
+++ index.js
@@ -8,3 +8,20 @@ function setCopyright() {
     }
 }
 setCopyright();
+
+
+$(".book").mouseenter(function(){
+    $(this).parent(".book-wrap").addClass("rotate");
+  });
+  
+  $(".book").mouseleave(function(){
+    $(this).parent(".book-wrap").removeClass("rotate");
+  });
+  
+  $(".book").click(function(){
+    $(this).parent(".book-wrap").addClass("flip");
+  });
+  
+  $(".book-back").click(function(){
+    $(this).parent(".book-wrap").removeClass("flip");
+  });
