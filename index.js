b0efc2db4ccbacaab4c1572077e9d5e599c2fe14
Krzysztof Kowerczyk <krzysztof.kowerczyk@gmail.com>
2021-05-15 17:13:34 +0200

    books 3d models implemented

--- index.js
+++ index.js
@@ -25,3 +25,51 @@ $(".book").mouseenter(function(){
   $(".book-back").click(function(){
     $(this).parent(".book-wrap").removeClass("flip");
   });
+/* dbs book */
+  $(".book-dbs").mouseenter(function(){
+    $(this).parent(".book-wrap").addClass("rotate");
+  });
+  
+  $(".book-dbs").mouseleave(function(){
+    $(this).parent(".book-wrap").removeClass("rotate");
+  });
+  
+  $(".book-dbs").click(function(){
+    $(this).parent(".book-wrap").addClass("flip");
+  });
+  
+  $(".book-back-dbs").click(function(){
+    $(this).parent(".book-wrap").removeClass("flip");
+  });
+  /* odwet book */
+  $(".book-odwet").mouseenter(function(){
+    $(this).parent(".book-wrap").addClass("rotate");
+  });
+  
+  $(".book-odwet").mouseleave(function(){
+    $(this).parent(".book-wrap").removeClass("rotate");
+  });
+  
+  $(".book-odwet").click(function(){
+    $(this).parent(".book-wrap").addClass("flip");
+  });
+  
+  $(".book-back-odwet").click(function(){
+    $(this).parent(".book-wrap").removeClass("flip");
+  });
+    /* zieloneSlonce book */
+    $(".book-zs").mouseenter(function(){
+      $(this).parent(".book-wrap").addClass("rotate");
+    });
+    
+    $(".book-zs").mouseleave(function(){
+      $(this).parent(".book-wrap").removeClass("rotate");
+    });
+    
+    $(".book-zs").click(function(){
+      $(this).parent(".book-wrap").addClass("flip");
+    });
+    
+    $(".book-back-zs").click(function(){
+      $(this).parent(".book-wrap").removeClass("flip");
+    });
